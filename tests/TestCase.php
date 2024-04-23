66b90e918de056238032f0a9074f4ef1bca97a8e
Daniel <info@lichtblau-it.de>
2024-04-23 07:58:37 +0200

    changed namespace

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Lancodev\\LunarPaypal\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Lichtblauit\\LunarPaypal\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
